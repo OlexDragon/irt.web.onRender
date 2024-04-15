160084e000dbb112a115fe26c60a54887c69cc44
Oleksandr Potomkin <potomkin@hotmail.com>
2024-04-15 12:37:35 -0400

    Fixed GIT problems

--- irt.web.test/src/main/java/irt/web/bean/jpa/RemoteAddressRepository.java
+++
@@ -1,6 +0,0 @@
-package irt.web.bean.jpa;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RemoteAddressRepository extends CrudRepository<RemoteAddress, String> {
-}
